adb7547f405485c38e216d1637b18598999ba18d
nnabiyev <xadfdo2ogjog36b3vo5bwuw4v65na7tnvg77443vwzxpxbvqwida>
2022-10-09 12:51:48 +0600

    [ADDED] libs versions fix

--- app/src/main/java/com/nurlandroid/kotapp/feature/posts/Post.kt
+++ app/src/main/java/com/nurlandroid/kotapp/feature/posts/Post.kt
@@ -3,7 +3,7 @@ package com.nurlandroid.kotapp.feature.posts
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Entity
 @Parcelize
@@ -11,4 +11,5 @@ data class Post(
     @PrimaryKey(autoGenerate = true) val id: Int,
     val title: String,
     var page: Int
-) : Parcelable
+)
+    : Parcelable
